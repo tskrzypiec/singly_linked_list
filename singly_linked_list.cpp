1ff9abeb228a8b9abd4419c949d529e57f670cf3
tskrzypiec <tomasz.t.skrzypiec@gmail.com>
2017-12-02 22:55:22 +0100

    ver.3

--- singly_linked_list.cpp
+++ singly_linked_list.cpp
@@ -9,66 +9,67 @@ struct list_element  // tworzymy strukture elementów listy
     int liczba;
 };
 
-// wyświetlamy zawartość elementów listy
+// wyświetl zawartość listy
 
 void show(list_element * p)
 {
     unsigned i;
     cout<<"Liczby które mamy to: "<<endl;
 
-    for(i = 1; p; p = p->next_element)                                  //W pętli przetwarzamy element wskazywany przez tą zmienną, po czym za nową wartość
-        cout << "Liczba  " << i++ << " to: " << p->liczba << endl;
-    cout << endl;                                                                  // zmiennej przyjmuje się adres następnego elementu listy.
-                                                                        //Adres ten jest przechowywany w polu next elementu bieżącego.
-                                                                        //Listę przechodzimy do momentu, aż zmienna wskazująca przyjmie wartość 0.
-                                                                        // Stanie się tak po wyjściu z ostatniego elementu listy, w którego polu next przechowywany jest adres 0.
+    for(i = 1; p; p = p->next_element)                                  // nową wartość zmiennej to adres następnego elementu listy (przechowywany w polu next bieżącego elementu)
+        cout << "Liczba  " << i++ << " to: " << p->liczba << endl;     // w ostatnim elemencie listy w polu next przechowywany jest adres 0
+    cout << endl;
+
 }
 
-// dodajemy element na początek listy
+// dodaj element na początek listy
 
 void push_front(list_element * & head, int ile)
 {
     list_element * p;
 
-    p = new list_element; //tworzymy dynamicznie w pamięci nowy element listy
-    (*p).liczba = ile;   //wprowadzamy wartość dla pola liczba // mozna stralka p=> liczba = ile;
-    p->next_element = head; // W polu next_element umieszczamy adres przechowywany przez zmienną head. W ten sposób następnikiem nowego elementu stanie się obecny pierwszy element listy.
-    head = p;  //w zmiennej head umieszczamy adres nowego elementu. Po tej operacji początkiem listy staje się nowo utworzony element.
+    p = new list_element; // tworzymy dynamicznie w pamięci nowy element listy
+    (*p).liczba = ile;    // wprowadzamy wartość dla pola liczba
+    p->next_element = head; // w polu next_element umieszczamy adres przechowywany przez zmienną head, następnym elementem stanie się obecny pierwszy element listy
+    head = p;  // w zmiennej head umieszczamy adres nowego elementu, początkiem listy staje się nowo utworzony element
 }
 
-// Procedura usuwa pierwszy element
-//---------------------------------
+// usuń element
+
 void pop_front(list_element * & head)
 {
-    list_element * p = head; //zapamiętujemy pierwszy element
+    list_element * p = head; // zapamiętujemy pierwszy element
 
-    if(p)  //zakończ jeśli lista jest pusta
+    if(p)  // zakończ jeśli lista jest pusta
     {
-        head = p->next_element; // w zmiennej head umieszczamy zawartość pola next usuwanego elementu. W ten sposób początek listy rozpocznie się w następniku, a usuwany element zostanie wyłączony z listy.
-        delete p; //usuwamy z pamięci
+        head = p->next_element; // w zmiennej head umieszczamy zawartość next_element usuwanego elementu, lista rozpocznie się od następnego elementu
+        delete p; // usuwamy z pamięci
     }
 }
-
-//---------------
-// Program główny
-//---------------
 
 int main()
 {
     list_element * L = NULL;
 
+    show(L);
+    cout<<"push x 3"<<endl<<endl;
     push_front(L,1);
     push_front(L,8);
     push_front(L,9);
     show(L);
+    cout<<"Teraz pop x 1"<<endl;
     pop_front(L);
     show(L);
+    cout<<"pop"<<endl;
     pop_front(L);
     show(L);
+    cout<<"Push"<<endl;
     push_front(L,8);
     show(L);
+    cout<<"Push"<<endl;
     push_front(L,9);
     show(L);
+    cout<<"Ok wystarczy"<<endl;
 
     return 0;
 }
