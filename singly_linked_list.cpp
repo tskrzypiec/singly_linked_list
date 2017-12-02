0e97d3dfbafdb9d446482c31f60ebb17f5b30e6b
tskrzypiec <tomasz.t.skrzypiec@gmail.com>
2017-12-02 13:17:01 +0100

    ver.2

--- singly_linked_list.cpp
+++ singly_linked_list.cpp
@@ -31,8 +31,8 @@ void push_front(list_element * & head, int ile)
     list_element * p;
 
     p = new list_element; //tworzymy dynamicznie w pamięci nowy element listy
-    p->liczba = ile;   //wprowadzamy wartość dla pola liczba
-    p->next_element = head; // W polu next_element elementu umieszczamy adres przechowywany przez zmienną head. W ten sposób następnikiem nowego elementu stanie się obecny pierwszy element listy.
+    (*p).liczba = ile;   //wprowadzamy wartość dla pola liczba // mozna stralka p=> liczba = ile;
+    p->next_element = head; // W polu next_element umieszczamy adres przechowywany przez zmienną head. W ten sposób następnikiem nowego elementu stanie się obecny pierwszy element listy.
     head = p;  //w zmiennej head umieszczamy adres nowego elementu. Po tej operacji początkiem listy staje się nowo utworzony element.
 }
 
@@ -40,9 +40,9 @@ void push_front(list_element * & head, int ile)
 //---------------------------------
 void pop_front(list_element * & head)
 {
-    list_element * p = head;
+    list_element * p = head; //zapamiętujemy pierwszy element
 
-    if(p)
+    if(p)  //zakończ jeśli lista jest pusta
     {
         head = p->next_element; // w zmiennej head umieszczamy zawartość pola next usuwanego elementu. W ten sposób początek listy rozpocznie się w następniku, a usuwany element zostanie wyłączony z listy.
         delete p; //usuwamy z pamięci
